d8c6b32cd35e3c782ce36724e5845000666f8b0f
Gayathri-051998 <gayathrivelayutham52@gmail.com>
2025-06-19 22:57:09 +0530

    commituseremail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const sendEmail = async (to, subject, text) => {
+  try {
     const transporter = nodemailer.createTransport({
       service: 'Gmail',
       auth: {
@@ -8,15 +9,19 @@ const sendEmail = async (to, subject, text) => {
         pass: process.env.EMAIL_PASS
       }
     });
-  
+
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to,
       subject,
       text,
     });
-  };
-  
 
-  
-  export default sendEmail;
+    console.log("✅ Email sent to:", to);
+
+  } catch (error) {
+    console.error("❌ Email send failed:", error.message);
+  }
+};
+
+export default sendEmail;
